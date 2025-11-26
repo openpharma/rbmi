26ab6a098116447d558bb53447d0a552c0624672
Isaac Gravestock <isaac.gravestock@roche.com>
2025-11-26 10:59:24 +0100

    try reg.finalizer

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,16 @@
 .onLoad <- function(...) {
     set_options()
 }
+
+#' Clean up after examples
+#' @keywords internal
+#' @examples
+#' \dontshow{
+#'   e <- environment(),  
+#'   reg.finalizer(e, f = tidy_up_examples, onexit = TRUE)
+#' }
+#' 
+tidy_up_examples <- function(e) {
+    clear_model_cache(keep = "")
+    rm(e)
+}
