c36a61499bf8a362a46b78af9ff63434d759aff3
gowercr1 <craig.gower-page@novartis.com>
2025-11-28 15:33:24 +0000

    additional grep checks

--- tests/scripts/rcmdcheck_donttest.sh
+++ tests/scripts/rcmdcheck_donttest.sh
@@ -17,6 +17,7 @@ temp_dir=$(mktemp -d)
 cd ${temp_dir}
 
 # R CMD CHECK args
+# https://cran.r-project.org/doc/manuals/r-release/R-ints.html#Tools
 export R_KEEP_PKG_SOURCE=yes
 export _R_CHECK_DONTTEST_EXAMPLES_=true
 export _R_CHECK_THINGS_IN_OTHER_DIRS_=true
@@ -31,8 +32,27 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-if grep -q "checking for new files in some other directories ... NOTE" ${temp_dir}/*.Rcheck/00check.log; then
+note_phrase="checking for new files in some other directories ... NOTE"
+if grep -q "${note_phrase}" ${temp_dir}/*.Rcheck/00check.log; then
     echo "ERROR: Found NOTE about new files in other directories"
+    exit 1
+fi
+
+# Check for status line
+if ! grep -q "^Status" ${temp_dir}/*.Rcheck/00check.log; then
+    echo "ERROR: No status line found in R CMD check log"
+    exit 1
+fi
+
+# Check for errors
+if grep -q "ERROR" ${temp_dir}/*.Rcheck/00check.log; then
+    echo "ERROR: R CMD check has errors"
+    exit 1
+fi
+
+# Check for warnings
+if grep -q "WARNING" ${temp_dir}/*.Rcheck/00check.log; then
+    echo "ERROR: R CMD check has warnings"
     exit 1
 fi
 
