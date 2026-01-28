d4c90822e3f97c1efd7621ebd1735f5eeddcadef
Lukas Widmer <lukas_andreas.widmer@novartis.com>
2026-01-28 18:04:14 +0100

    Quote str_pad

--- R/as_ascii_table.R
+++ R/as_ascii_table.R
@@ -1,6 +1,6 @@
 #' string_pad
 #'
-#' Utility function used to replicate str_pad. Adds white space to either end
+#' Utility function used to replicate `str_pad`. Adds white space to either end
 #' of a string to get it to equal the desired length
 #' @param x string
 #' @param width desired length
