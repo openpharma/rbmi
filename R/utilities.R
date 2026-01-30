d78bcdea0701de4af78afc13f8328d8d9331ab92
Lukas Widmer <lukas_andreas.widmer@novartis.com>
2026-01-30 09:47:48 +0100

    Quote dplyr

--- R/utilities.R
+++ R/utilities.R
@@ -532,7 +532,7 @@ format_method_descriptions <- function(method) {
 #' @param x a data.frame like object
 #'
 #' Utility function to convert a "data.frame-like" object to an actual `data.frame`
-#' to avoid issues with inconsistency on methods (such as  `[`() and dplyr's grouped dataframes)
+#' to avoid issues with inconsistency on methods (such as  `[`() and `dplyr`'s grouped dataframes)
 as_dataframe <- function(x) {
     x2 <- as.data.frame(x)
     row.names(x2) <- NULL
