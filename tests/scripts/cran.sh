020c4c6e639cd18f43b00180d2cbe6879769116d
Lukas Widmer <lukas_andreas.widmer@novartis.com>
2026-02-26 10:47:57 +0100

    Echo by default adds a trailing newline

--- tests/scripts/cran.sh
+++ tests/scripts/cran.sh
@@ -19,7 +19,7 @@ cd ${temp_dir}
 
 
 if [[ "${RUNNER_CRAN_OVERRIDE}" == "true" || "${RUNNER_CRAN_OVERRIDE}" == "TRUE" || "${RUNNER_CRAN_OVERRIDE}" == "1" ]]; then
-  echo 'options(repos = c(CRAN = "https://cloud.r-project.org"))\n' >> ".Rprofile"
+  echo 'options(repos = c(CRAN = "https://cloud.r-project.org"))' >> ".Rprofile"
   echo "CRAN URL override enabled"
 fi
 
