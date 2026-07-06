93856eb439b5af50916be09cf01bcbf58b067768
Lukas Widmer <lukas_andreas.widmer@novartis.com>
2026-07-06 18:09:35 +0200

    Document return value of locf

--- R/expand.R
+++ R/expand.R
@@ -7,6 +7,9 @@
 #' \dontrun{
 #' locf(c(NA, 1, 2, 3, NA, 4)) # Returns c(NA, 1, 2, 3, 3, 4)
 #' }
+#' @return A vector of the same length as `x` in which each `NA` has been
+#' replaced by the most recent preceding non-`NA` value. Leading `NA`s (those
+#' with no earlier value to carry forward) are left unchanged.
 #' @export
 locf <- function(x) {
     inds <- cumsum(!is.na(x))
